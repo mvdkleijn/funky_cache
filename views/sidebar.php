bbf04201504c434f7eaac2966a081e85e6c7534e
Martijn <martijn.niji@gmail.com>
2012-01-20 23:58:33 +0100

    Fixed icon paths

--- views/sidebar.php
+++ views/sidebar.php
@@ -11,6 +11,6 @@
  * For details, see:  http://www.opensource.org/licenses/mit-license.php
  */
 ?>
-<p class="button"><a href="<?php echo get_url('plugin/funky_cache/'); ?>"><img src="/wolf/plugins/funky_cache/images/page_copy.png" align="middle" alt="page icon" /> <?php echo __('Cached pages'); ?></a></p>
-<p class="button"><a href="<?php echo get_url('plugin/funky_cache/settings'); ?>"><img src="/wolf/plugins/funky_cache/images/pencil.png" align="middle" alt="page icon" /> <?php echo __('Settings'); ?></a></p>
-<p class="button"><a href="<?php echo get_url('plugin/funky_cache/documentation/'); ?>"><img src="/wolf/plugins/funky_cache/images/help.png" align="middle" alt="page icon" /> <?php echo __('Your .htaccess file'); ?></a></p>
+<p class="button"><a href="<?php echo get_url('plugin/funky_cache/'); ?>"><img src="<?php echo ICONS_URI;?>/file-folder-32.png" align="middle" alt="folder icon" /><?php echo __('Cached pages'); ?></a></p>
+<p class="button"><a href="<?php echo get_url('plugin/funky_cache/documentation/'); ?>"><img src="<?php echo ICONS_URI;?>/file-unknown-32.png" align="middle" alt="page icon" /><?php echo __('Example rewrite rules'); ?></a></p>
+<p class="button"><a href="<?php echo get_url('plugin/funky_cache/settings'); ?>"><img src="<?php echo ICONS_URI;?>/settings-32.png" align="middle" alt="settings icon" /><?php echo __('Settings'); ?></a></p>
