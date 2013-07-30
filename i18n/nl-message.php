15cd4db3323002cc724fdde7a712ab623920e655
staticgeek <staticgeek@gmail.com>
2013-07-30 09:28:00 -0500

    Create nl-message.php
    Translated via Google Translate

---
+++ i18n/nl-message.php
@@ -0,0 +1,59 @@
+<?php
+
+    /**
+* Language file for plugin funky_cache
+*
+* @package Plugins
+* @subpackage funky_cache
+*
+* @author @frontenderch
+* @version Wolf 0.7.7
+*/
+
+    return array(
+        // index.php
+        'Cache' => 'Cache',
+        'Should cache' => 'Mocht cache',
+
+        // views/index.php
+        'Funky Cache' => 'Funky Cache',
+        'Cached pages' => 'Cached pagina's',
+        'Clear all' => 'In orde',
+
+        // views/documentation.php
+        'Funky Cache - Example rewrite rules' => 'Funky Cache - Voorbeeld herschrijfregels',
+        'Introduction' => 'Introductie',
+        'The Funky Cache plugin works by using mod_rewrite or equivalent rewrite functionality. Below you will find generated examples for the most used HTTP servers.' => 'De Funky Cache plugin werkt met behulp van mod_rewrite of gelijkwaardig herschrijven functionaliteit. Hieronder vindt u voorbeelden gegenereerd voor de meest gebruikte HTTP-servers te vinden.',
+        'Please be aware the author of this plugin cannot guarantee the accuracy of these examples and does not know all rewrite systems.' => 'Wees u ervan bewust de auteur van deze plugin kan niet instaan ??voor de juistheid van deze voorbeelden en weet niet alle herschrijfsystemen.',
+        'Always check the plugin settings after enabling it!' => 'Controleer altijd de plugin instellingen na het inschakelen van het!',
+        'If you have translated the rewrite rules to another platform, please let the maintainer of this plugin know.' => 'Als u de herschrijfregels naar een ander platform te hebben omgezet, laat de eigenaar van deze plugin kennen.',
+
+        // views/settings.php
+        'Funky Cache Plugin' => 'Funky Cache Plugin',
+        'Cache settings' => 'Cache-instellingen',
+        'Cache by default' => 'Cache standaard',
+        'Yes' => 'Ja',
+        'No' => 'Geen',
+        'Choose yes if you want your pages to be cached by default. Otherwise you must set caching for each page manually.' => 'Kies Ja als u wilt dat uw pagina's worden gecached door gebrek. Anders moet u caching voor elke pagina handmatig.',
+        'Cache file suffix' => 'Cache-bestand achtervoegsel',
+        'Suffix for cache files written to disk. If you use other than .html you also need to update your mod_rewrite rules.' => 'Achtervoegsel voor cache-bestanden naar de schijf geschreven. Als je dan. Html gebruiken heb je ook nodig hebt om uw mod_rewrite regels werken.',
+        'Cache folder' => 'Cache map',
+        'Folder where static cache files are written. Relative to document root. When you change this you also need to update your mod_rewrite rules.' => 'Map waar statische cache bestanden worden geschreven. Ten opzichte van document root. Wanneer u deze wijzigt u ook nodig hebt om uw mod_rewrite regels werken.',
+        'Save' => 'sparen',
+
+        // views/sidebar.php
+        'Example rewrite rules' => 'Voorbeeld herschrijfregels',
+        'Settings' => 'Instellingen',
+        'About Funky Cache' => 'Over Funky Cache',
+        'The Funky Cache plugin allows you to cache pages on the filesystem. It works by using mod_rewrite or equivalent rewrite functionality.' => 'De Funky Cache plugin maakt het mogelijk om pagina's op het bestandssysteem cache. Het werkt met behulp van mod_rewrite of gelijkwaardig herschrijven functionaliteit.',
+        'Homepage' => 'Primaire pagina',
+
+        // FunkyCacheController.php
+        'Page was deleted from cache.' => 'Pagina werd verwijderd uit de cache.',
+        'The cached page could not be deleted. Try manually from the commandline.' => 'De opgeslagen pagina kon niet worden verwijderd. Probeer handmatig vanaf de commandline.',
+        'Cache cleared successfully.' => 'Cache gewist succes.',
+        'One or more cached pages could not be deleted. Try manually from the commandline.' => 'Een of meer pagina in de cache kon niet worden verwijderd. Probeer handmatig vanaf de commandline.',
+        'The cache settings have been updated.' => 'De cache-instellingen zijn bijgewerkt.',
+        'The cache settings could not be updated due to an error.' => 'De cache-instellingen kon niet vanwege een fout worden bijgewerkt. ',
+
+    );
